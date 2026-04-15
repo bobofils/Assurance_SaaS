fc6ec389a56ef9529fcb289e0498245be4afc136
BOBO <bobofilsgloireadieu@gmail.com>
2026-04-15 11:48:52 +0200

    fix authentication login not working

--- auth.py
+++ auth.py
@@ -2,42 +2,50 @@ import streamlit as st
 from database import create_user, get_user
 from security import check_password
 
-
+# =========================
+# AUTH PAGE
+# =========================
 def auth_page():
-    st.sidebar.title("🔐 Authentification")
 
-    menu = st.sidebar.radio("Menu", ["Login", "Signup"])
+    st.title("🔐 Authentification")
 
-    # =====================
+    menu = st.radio("Menu", ["Login", "Signup"])
+
+    # =========================
     # SIGNUP
-    # =====================
+    # =========================
     if menu == "Signup":
-        st.subheader("📝 Créer un compte")
+        st.subheader("Créer un compte")
 
-        username = st.text_input("Nom utilisateur")
-        password = st.text_input("Mot de passe", type="password")
+        username = st.text_input("Utilisateur", key="su_user")
+        password = st.text_input("Mot de passe", type="password", key="su_pass")
 
         if st.button("Créer compte"):
+
             if create_user(username, password):
                 st.success("Compte créé avec succès ✅")
             else:
-                st.error("Utilisateur existe déjà ❌")
+                st.error("Utilisateur déjà existant ❌")
 
-    # =====================
+    # =========================
     # LOGIN
-    # =====================
-    if menu == "Login":
-        st.subheader("🔑 Connexion")
+    # =========================
+    else:
+        st.subheader("Connexion")
 
-        username = st.text_input("Utilisateur")
-        password = st.text_input("Mot de passe", type="password")
+        username = st.text_input("Utilisateur", key="li_user")
+        password = st.text_input("Mot de passe", type="password", key="li_pass")
 
         if st.button("Se connecter"):
+
             user = get_user(username)
 
             if user and check_password(password, user[2]):
+
                 st.session_state["user"] = username
                 st.success("Connexion réussie ✅")
+
                 st.rerun()
+
             else:
-                st.error("Identifiants incorrects ❌")
+                st.error("Login incorrect ❌")
